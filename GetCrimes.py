3472af2b000b09bb9c5fb81da33b13b77a2ab202
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-04-08 22:07:00 -0600

    Added some documentation
    Will continue this in a proper ide

--- GetCrimes.py
+++ GetCrimes.py
@@ -1,51 +1,53 @@
 '''FILENAME: GetCrimes.py
-LIBRARY NAME: CPD Crime Access
 CREATED BY: Nicholas M. Synovic
-GITHUB: https://www.github.com/nsynovic
-LAST EDITED: 4/3/2019 14:09
+GITHUB URL: https://www.github.com/nsynovic
 
-ABSTRACTION: This libary uses the CPD Crimes OpenData Socrata API to download JSON files contating crime information.
+ABSTRACTION: This libary uses the CPD Crimes OpenData Socrata API to download JSON files contating crime reports.
 '''
+import json # Used to save JSON data to files.
+from urllib.request import urlopen  # Used to access and download JSON data.
 
-import json # Used to save JSON data to files
-from urllib.request import urlopen  # Used to access and download JSON data 
 
 class GetCrimes:
-    '''ABSTRACTION: Class contains methods used to download and save JSON files.'''
-    def __init__(self, appToken:str=None):
-        '''ABSTRACTION:
-PRE:
-POST:
+    '''ABSTRACTION: Creates a tunnel to the OpenData Socrate API to get crime reports.
 '''
-        self.appToken = appToken    # Not needed, however better stability with it 
-
+    def __init__(self, appToken:str="")   ->  None:
+        '''ABSTRACTION: Starts the class with or without an OpenData app token, and initializes class variables.
+PRE: Takes in an app token string.
+POST: Creates a class variable that contains the app token and all other needed class variables with their default values.
+'''
+        self.appToken = appToken    # Not needed, however better stability with it.
+        self.offset = 0
+        self.url = ""
+        self.json = {}
+        
     def getAppToken(self)   ->  str:
-        '''ABSTRACTION:
-PRE:
-POST:
+        '''ABSTRACTION: Returns the app token class variable
+PRE: No inputs.
+POST: Returns app token class variable.
 '''
         return self.appToken
 
     def getOffset(self) ->  int:
-        '''ABSTRACTION:
-PRE:
-POST:
+        '''ABSTRACTION: Returns the offset class variable.
+PRE: No inputs.
+POST: Returns offset class variable.
 '''
         return self.offset
     
-    def getURLJSON(self, offset:int=None)   ->  dict:
-        '''ABSTRACTION:
-PRE:
-POST:
+    def setJSON(self, offset:int=0)   ->  None:
+        '''ABSTRACTION: Stores crime data from in a dictionary in memory. 
+PRE: offset is where the crime count will start from (i.e get crimes after the 50th [offest=50] crime).
+POST: Stores the crime report JSON in a dictionary.
 '''
         self.setURLString(offset=offset)
         data = urlopen(url=self.getURLString())
-        return json.load(fp=data)
-
+        self.json = json.load(fp=data)
+        
     def getURLString(self) ->  str:
-        '''ABSTRACTION:
-PRE:
-POST:
+        '''ABSTRACTION: Returns the url class variable.
+PRE: No inputs.
+POST: Returns url class variable.
 '''        
         return self.url
 
