83854bc6136eaf7fc9b34f85a68104b00c1277c7
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-04-09 00:53:28 -0600

    Added proper documentation
    Hey look documentation! Woot woot!!
    More to come soon.

--- DownloadCrimeFiles.py
+++ DownloadCrimeFiles.py
@@ -1,28 +1,35 @@
 '''FILENAME: DownloadCrimeFiles.py
-PROGRAM NAME: CPD Crime Information Downloader
 CREATED BY: Nicholas M. Synovic
-GITHUB: https://www.github.com/nsynovic
-LAST EDITED: 4/3/2019 14:09
+GITHUB URL: https://www.github.com/nsynovic
+
+ABSTRACTION: Test application of the GetCrimes.py class GetCrimes. Really is only useful for understanding how to use the GetCrime library of code, or for building your own crime database.
 '''
-import GetCrimes
-def download(appToken:str=None, crimeRange:int=6836, startingOffset:int=0)   ->  None:
-    gc = GetCrimes.GetCrimes(appToken=appToken)
-    for count in range(crimeRange):
-        offset = count * 1000
-        if offset < startingOffset:
-            pass
-        else:
-            filename = r"JSON\Crimes\crimes" + str(count) + ".json"
-            data = gc.getURLJSON(offset=offset)
-            print(str(gc.saveJSON(filename=filename, data=data)) + " " + gc.getURLString())
+from GetCrimes import GetCrimes
+import math
 
-print("""CPD Crime Downloader
-Created by Nicholas Synovic
-""")
-aT = input("What is your SODA app token? ")
-cR = input("How many files do you want to download (NOTE: one file has a thousand crimes in it)? ")
-sO = input("What is your crime offset (NOTE: enter 0 if you don't know)? ")
-print("""Starting download...
-""")
+aT = input("What is your app token? ")
+# oS = int(input("What offset do you want your crimes to start at? "))
+oS = 0
+mX = int(input("What is the maximum amount of iterations that you want this program to run (NOTE: Each iteration will create 1 file)? "))
 
-download(appToken=aT, crimeRange=cR, startingOffset=sO)
+# gc = GetCrimes(appToken=aT, offset=oS)
+gc = GetCrimes(appToken=aT, offset=oS)
+
+for x in range(mX):
+    new = 0
+    filename = r"JSON\Crimes\crimes" + str(x) + ".json" 
+    if oS == 0:
+        gc.setURL(appToken=aT, offset=x*1000)
+        gc.setJSON()    # Downloads json
+        data = gc.getJSON() # Makes json viewable
+        outcome1 = gc.saveJSON(filename=filename, data=data)
+        outcome2 = gc.getURL()
+        print(str(x) + ", " + str(outcome1) + ", " + outcome2)
+
+    # else:    
+    #     gc.setJSON()    # Downloads json
+    #     data = gc.getJSON() # Makes json viewable
+    #     outcome1 = gc.saveJSON(filename=filename, data=data)
+    #     outcome2 = gc.getURL()
+    #     print(str(x) + ", " + str(outcome1) + ", " + outcome2)
+    #     gc.setURL(appToken=aT, offset=((oS + 1 + x * 1000)))
